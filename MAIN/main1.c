bf2b1cac8b9169bc2ca22dad267c29895533681d
it61070132 <44919308+it61070132@users.noreply.github.com>
2019-04-30 20:06:40 +0700

    fix

--- MAIN/main1.c
+++ MAIN/main1.c
@@ -2,47 +2,6 @@
 
 int fchoice;
 
-int main()
-{
-	installdatabase();
-	printf("|>>> INCOME/OUTCOME PROGRAM <<<|\n");
-	printf("|>>> SELECT TOPICS <<<|\n");
-	printf("| 1.) SELECT MONTH \n" );
-	printf("| 2.) REMAINING MONEY \n");
-	printf("| 3.) TOTAL INCOME \n");
-	printf("| 4.) TOTAL EXPENSES \n");
-	printf("| 5.) EXIT PROGRAM \n");
-	scanf("%d",&fchoice);
-	if (fchoice == 1)
-	{
-		printf("|>>> Input your month number <<<|\n");
-		printf("|>>> Number Only <<<|\n");
-		printf("| 1.) January\n");
-		printf("| 2.) February\n");
-		printf("| 3.) March\n");
-		printf("| 4.) April\n");
-		printf("| 5.) May\n");
-		printf("| 6.) June\n");
-		printf("| 7.) July\n");
-		printf("| 8.) August\n");
-		printf("| 9.) September\n");
-		printf("| 10.) October\n");
-		printf("| 11.) November\n");
-		printf("| 12.) December\n");
-		month();
-	}
-	if (fchoice == 2){
-		printf("Remaining Money: %d",totalmoney);
-	}
-	if (fchoice == 3){
-		printf("Total Income: %d",totalincome);
-	}
-	if (fchoice == 4){
-		printf("Total Expenses: %d",totalexpenses);
-	}
-	return 0;
-}
-
 void month(){
 	int cmonth;
 	scanf("%d", &cmonth);
@@ -110,5 +69,48 @@ void month(){
 	{
 		printf("Error\n");
 	}
+
+int main()
+{
+	installdatabase();
+	printf("|>>> INCOME/OUTCOME PROGRAM <<<|\n");
+	printf("|>>> SELECT TOPICS <<<|\n");
+	printf("| 1.) SELECT MONTH \n" );
+	printf("| 2.) REMAINING MONEY \n");
+	printf("| 3.) TOTAL INCOME \n");
+	printf("| 4.) TOTAL EXPENSES \n");
+	printf("| 5.) EXIT PROGRAM \n");
+	scanf("%d",&fchoice);
+	if (fchoice == 1)
+	{
+		printf("|>>> Input your month number <<<|\n");
+		printf("|>>> Number Only <<<|\n");
+		printf("| 1.) January\n");
+		printf("| 2.) February\n");
+		printf("| 3.) March\n");
+		printf("| 4.) April\n");
+		printf("| 5.) May\n");
+		printf("| 6.) June\n");
+		printf("| 7.) July\n");
+		printf("| 8.) August\n");
+		printf("| 9.) September\n");
+		printf("| 10.) October\n");
+		printf("| 11.) November\n");
+		printf("| 12.) December\n");
+		month();
+	}
+	if (fchoice == 2){
+		printf("Remaining Money: %d",totalmoney);
+	}
+	if (fchoice == 3){
+		printf("Total Income: %d",totalincome);
+	}
+	if (fchoice == 4){
+		printf("Total Expenses: %d",totalexpenses);
+	}
+	return 0;
+}
+
+
 
 }
