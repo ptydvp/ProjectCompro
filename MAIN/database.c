b3c82f2816b3532bd1888eb233a8c0d0cb09b7ac
it61070132 <44919308+it61070132@users.noreply.github.com>
2019-05-01 21:41:55 +0700

    fix

--- MAIN/database.c
+++ MAIN/database.c
@@ -592,11 +592,11 @@ void topic() {
 }
 
 void urfinance() {
-    printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
-    printf("::                     â–ˆ    â–ˆ  â–ˆâ–ˆâ–ˆâ–ˆ  â–ˆâ–ˆâ–ˆâ–ˆâ–ˆ â–ˆâ–ˆâ–ˆ  â–ˆ   â–ˆ   â–ˆâ–ˆâ–ˆ  â–ˆ   â–ˆ  â–ˆâ–ˆâ–ˆâ–ˆ  â–ˆâ–ˆâ–ˆ  â–ˆâ–ˆâ–ˆ  â–ˆ                        ::\n");
-    printf("::                     â–ˆ    â–ˆ  â–ˆ   â–ˆ â–ˆ      â–ˆ   â–ˆâ–ˆ  â–ˆ  â–ˆ   â–ˆ â–ˆâ–ˆ  â–ˆ â–ˆ       â–ˆ  â–ˆ   â–ˆ â–ˆ                        ::\n");
-    printf("::                     â–ˆ    â–ˆ  â–ˆâ–ˆâ–ˆâ–ˆ  â–ˆâ–ˆâ–ˆ    â–ˆ   â–ˆ â–ˆ â–ˆ  â–ˆâ–ˆâ–ˆâ–ˆâ–ˆ â–ˆ â–ˆ â–ˆ â–ˆ       â–ˆ  â–ˆâ–ˆâ–ˆâ–ˆâ–ˆ â–ˆ                        ::\n");
-    printf("::                     â–ˆâ–ˆâ–ˆâ–ˆâ–ˆâ–ˆ  â–ˆ   â–ˆ â–ˆ     â–ˆâ–ˆâ–ˆ  â–ˆ  â–ˆâ–ˆ  â–ˆ   â–ˆ â–ˆ  â–ˆâ–ˆ  â–ˆâ–ˆâ–ˆâ–ˆ  â–ˆâ–ˆâ–ˆ â–ˆ   â–ˆ â–ˆâ–ˆâ–ˆâ–ˆâ–ˆ                    ::\n");
+printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
+    printf("::                     ¦    ¦  ¦¦¦¦  ¦¦¦¦¦ ¦¦¦  ¦   ¦   ¦¦¦  ¦   ¦  ¦¦¦¦  ¦¦¦  ¦¦¦  ¦                        ::\n");
+    printf("::                     ¦    ¦  ¦   ¦ ¦      ¦   ¦¦  ¦  ¦   ¦ ¦¦  ¦ ¦       ¦  ¦   ¦ ¦                        ::\n");
+    printf("::                     ¦    ¦  ¦¦¦¦  ¦¦¦    ¦   ¦ ¦ ¦  ¦¦¦¦¦ ¦ ¦ ¦ ¦       ¦  ¦¦¦¦¦ ¦                        ::\n");
+    printf("::                     ¦¦¦¦¦¦  ¦   ¦ ¦     ¦¦¦  ¦  ¦¦  ¦   ¦ ¦  ¦¦  ¦¦¦¦  ¦¦¦ ¦   ¦ ¦¦¦¦¦                    ::\n");
     printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
 }
 
@@ -605,7 +605,6 @@ void endbar() {
 }
 
 void firstpage() {
-    installdatabase();
 	urfinance();
 	printf("::                                   >>> INCOME/OUTCOME PROGRAM <<<                                          ::\n");
     printf("::                                        >>> SELECT TOPICS <<<                                              ::\n");
@@ -616,6 +615,50 @@ void firstpage() {
 	printf("::                                          5.) EXIT PROGRAM                                                 ::\n");
 	endbar();
 	scanf("%d",&fchoice);
+	if (fchoice == 1)
+	{
+		urfinance();
+    	printf("::                                 >>> Input your month number <<<                                           ::\n");
+		printf("::                                      >>> Number Only <<<                                                  ::\n");
+		printf("::                                          1.) January                                                      ::\n");
+		printf("::                                          2.) February                                                     ::\n");
+		printf("::                                          3.) March                                                        ::\n");
+		printf("::                                          4.) April                                                        ::\n");
+  		printf("::                                          5.) May                                                          ::\n");
+		printf("::                                          6.) June                                                         ::\n");
+		printf("::                                          7.) July                                                         ::\n");
+		printf("::                                          8.) August                                                       ::\n");
+		printf("::                                          9.) September                                                    ::\n");
+		printf("::                                          10.) October                                                     ::\n");
+		printf("::                                          11.) November                                                    ::\n");
+		printf("::                                          12.) December                                                    ::\n");
+ 	    endbar();
+		month();
+	}
+	if (fchoice == 2) {
+		endbar();
+		printf("::                                          Remaining Money: %d                                               ::\n",totalmoney);
+		endbar();
+		firstpage();
+	}
+	if (fchoice == 3) {
+		endbar();
+		printf("::                                       Total Income: %d  				                     ::\n",totalincome);
+		endbar();
+		firstpage();
+	}
+	if (fchoice == 4) {
+		endbar();
+		printf("::                                           Total Expenses: %d                                               ::\n",totalexpenses);
+		endbar();
+		firstpage();
+	}
+	if (fchoice == 5) {
+		urfinance();
+		printf("::			                         THANK YOU						     ::\n");
+		endbar();
+	}
+	return 0;
 }
 
 void month() {
